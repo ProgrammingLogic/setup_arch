ad6fd62defcfa1097890c2dcb2841acb56533b26
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2024-02-18 12:11:35 -0600

    Update zsh setup script with theme and plugins

--- applications/zsh/setup.sh
+++ applications/zsh/setup.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
+
+plugins=(git zsh-autosuggestions zsh-syntax-highlighting autojump)
+theme="amuse"
+
 # Copy the zshrc file to ~/.zshrc
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+# Set theme
+sed -i "s/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"$theme\"/" ~/.zshrc
+
+
+# Set plugins
+sed -i "s/plugins=(git)/plugins=(${plugins[*]})/" ~/.zshrc
+
+
+# Append custom .zshrc options to the end of the file
