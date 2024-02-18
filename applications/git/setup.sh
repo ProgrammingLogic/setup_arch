b067f08530b1246f7685a1c91af338e88099f4e9
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2024-02-18 12:21:29 -0600

    Add Git setup script

---
+++ applications/git/setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# FILEPATH: /home/jstiverson/Projects/setup_arch/applications/git/setup.sh
+
+# Check if Git is installed
+if command -v git &> /dev/null; then
+    echo "Git is installed."
+else
+    echo "Git is not installed."
+fi
+
+read -p "Please enter your email: " email
+if [[ $email =~ ^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$ ]]; then
+    echo "Email is valid, continuing..."
+else
+    echo "Email is invalid."
+fi
+
+read -p "Please enter your name: " name
+if [[ $name =~ ^[A-Za-z]+$ ]]; then
+    echo "Name is valid, continuing..."
+else
+    echo "Name is invalid."
+fi
+
+read -p "Do you want to use vim as the default editor? (y/n): " use_vim
+if [[ $use_vim == "y" ]]; then
+    echo "Using vim as the default editor."
+else
+    read -p "Please enter the name of the editor you want to use: " editor
+    echo "Using $editor as the default editor."
+fi
+
+
+git config --global user.email "$email"
+git config --global user.name "$name"
+git config --global core.editor "$editor"
