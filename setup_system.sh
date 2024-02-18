6dd448e185db9138f8e0eaccf598df9d7ef060be
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2024-02-18 12:36:54 -0600

    Add setup script for Arch Linux installation

--- setup_system.sh
+++ setup_system.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+# This script is meant to be run on a fresh Arch Linux installation
+# It will install the necessary packages and set up the system
+# It is meant to be run as root
+
+
 pacman -Sy --noconfirm archinstall
 archinstall --config ./archinstall/user_configuration.json
 
